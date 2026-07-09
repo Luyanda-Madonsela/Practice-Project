a93ff1756fd2fa5a88c8a42315445d6d4089b21c
Luyanda Madonsela <luyandasmadonsela@gmail.com>
2026-07-09 16:08:03 +0200

    Add new pages and layouts for various practice components; include Chakra UI elements

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -10,13 +10,13 @@ export default function Home() {
       
       <Grid templateColumns="repeat(2, 1fr)" gap={6} mt={8} h="20rem">
         <Button as="a" href="/chakracomponents" w="14rem" h="3.5rem" colorScheme="blue" variant="solid" size="lg">Chakra UI Components</Button>
-        <Button as="a" href="/chakracomponents" w="14rem" h="3.5rem" colorScheme="blue" variant="solid" size="lg">Images Practice</Button>
-        <Button as="a" href="/chakracomponents" w="14rem" h="3.5rem" colorScheme="blue" variant="solid" size="lg">Text Practice </Button>
-        <Button as="a" href="/chakracomponents" w="14rem" h="3.5rem" colorScheme="blue" variant="solid" size="lg">Date & Time Practice</Button>
-        <Button as="a" href="/chakracomponents" w="14rem" h="3.5rem" colorScheme="blue" variant="solid" size="lg">Video Display Practice</Button>
-        <Button as="a" href="/chakracomponents" w="14rem" h="3.5rem" colorScheme="blue" variant="solid" size="lg">Handling Documents Practice</Button>
-        <Button as="a" href="/chakracomponents" w="14rem" h="3.5rem" colorScheme="blue" variant="solid" size="lg">Interactive Map Practice</Button>
-        <Button as="a" href="/chakracomponents" w="14rem" h="3.5rem" colorScheme="blue" variant="solid" size="lg">Dark/ Light Mode Practice</Button>
+        <Button as="a" href="/imagespractice" w="14rem" h="3.5rem" colorScheme="blue" variant="solid" size="lg">Images Practice</Button>
+        <Button as="a" href="/textpractice" w="14rem" h="3.5rem" colorScheme="blue" variant="solid" size="lg">Text Practice </Button>
+        <Button as="a" href="/datetimepractice" w="14rem" h="3.5rem" colorScheme="blue" variant="solid" size="lg">Date & Time Practice</Button>
+        <Button as="a" href="/videodisplaypractice" w="14rem" h="3.5rem" colorScheme="blue" variant="solid" size="lg">Video Display Practice</Button>
+        <Button as="a" href="/documentspractice" w="14rem" h="3.5rem" colorScheme="blue" variant="solid" size="lg">Handling Documents Practice</Button>
+        <Button as="a" href="/interactivemap" w="14rem" h="3.5rem" colorScheme="blue" variant="solid" size="lg">Interactive Map Practice</Button>
+        <Button as="a" href="/darklightmode" w="14rem" h="3.5rem" colorScheme="blue" variant="solid" size="lg">Dark/ Light Mode Practice</Button>
       </Grid>
 
     </VStack>
