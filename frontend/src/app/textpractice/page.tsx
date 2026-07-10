cdc4606bb0e366801d323bbc61e5aced31c71582
Luyanda Madonsela <luyandasmadonsela@gmail.com>
2026-07-10 11:14:08 +0200

    Update TextPractice component to standardize width of sections to 90%

--- frontend/src/app/textpractice/page.tsx
+++ frontend/src/app/textpractice/page.tsx
@@ -4,11 +4,11 @@ import { Text, Heading, Spacer, Button, Box } from "@chakra-ui/react";
 export default function TextPractice() {
   return (
     <VStack minH="90vh">
-      <HStack borderWidth="1px" borderColor="red.500" w="95%" mt="3.75rem" alignItems="center">  
+      <HStack borderWidth="1px" borderColor="red.500" w="90%" mt="3.75rem" alignItems="center">  
         <Heading fontSize="4xl" fontWeight="bold">Text Practice</Heading>  
       </HStack>
       {/*------------------------------------------FONT SELECTOR---------------------------------------- */}
-      <VStack borderWidth="1px" borderColor="red.500" w="95%" h="13rem" mt="3rem" alignItems="center" justifyContent="start">
+      <VStack borderWidth="1px" borderColor="red.500" w="90%" h="13rem" mt="3rem" alignItems="center" justifyContent="start">
         <HStack borderWidth="1px" borderColor="red.500" w="98%" mt="2rem"> 
           <Text>Font Selector</Text>
           <Spacer/>
@@ -21,7 +21,7 @@ export default function TextPractice() {
         </Box>
       </VStack>
       {/*------------------------------------------HEADERS---------------------------------------- */}
-      <VStack borderWidth="1px" borderColor="red.500" w="95%" h="18rem" mt="3rem" alignItems="center" justifyContent="start">
+      <VStack borderWidth="1px" borderColor="red.500" w="90%" h="18rem" mt="3rem" alignItems="center" justifyContent="start">
         <HStack borderWidth="1px" borderColor="red.500" w="98%" mt="2rem"> 
           <Text>Headers(H1-H6)</Text>
           <Spacer/>
@@ -31,7 +31,7 @@ export default function TextPractice() {
         </Box>
       </VStack>
       {/*------------------------------------------TEXT AREA---------------------------------------- */}
-      <VStack borderWidth="1px" borderColor="red.500" w="95%" h="13rem" mt="3rem" alignItems="center" justifyContent="start">
+      <VStack borderWidth="1px" borderColor="red.500" w="90%" h="13rem" mt="3rem" alignItems="center" justifyContent="start">
         <HStack borderWidth="1px" borderColor="red.500" w="98%" mt="2rem"> 
           <Text>Text Area</Text>
           <Spacer/>
@@ -44,7 +44,7 @@ export default function TextPractice() {
         </Box>
       </VStack>
       {/*------------------------------------------TEXT FORMATTING---------------------------------------- */}
-      <VStack borderWidth="1px" borderColor="red.500" w="95%" h="13rem" mt="3rem" alignItems="center" justifyContent="start">
+      <VStack borderWidth="1px" borderColor="red.500" w="90%" h="13rem" mt="3rem" alignItems="center" justifyContent="start">
         <HStack borderWidth="1px" borderColor="red.500" w="98%" mt="2rem"> 
           <Text>Text Formatting</Text>
           <Spacer/>
@@ -57,7 +57,7 @@ export default function TextPractice() {
         </Box>
       </VStack>
       {/*------------------------------------------LISTS---------------------------------------- */}
-      <VStack borderWidth="1px" borderColor="red.500" w="95%" h="15rem" mt="3rem" alignItems="center" justifyContent="start">
+      <VStack borderWidth="1px" borderColor="red.500" w="90%" h="15rem" mt="3rem" alignItems="center" justifyContent="start">
         <HStack borderWidth="1px" borderColor="red.500" w="98%" mt="2rem"> 
           <Text>Lists (Ordered & Unordered)</Text>
           <Spacer/>
@@ -72,7 +72,7 @@ export default function TextPractice() {
         </HStack>
       </VStack>
       {/*------------------------------------------DIVIDERS---------------------------------------- */}
-      <VStack borderWidth="1px" borderColor="red.500" w="95%" h="10rem" mt="3rem" mb="9rem" alignItems="center" justifyContent="start">
+      <VStack borderWidth="1px" borderColor="red.500" w="90%" h="10rem" mt="3rem" mb="9rem" alignItems="center" justifyContent="start">
         <HStack borderWidth="1px" borderColor="red.500" w="98%" mt="2rem"> 
           <Text>Dividers</Text>
           <Spacer/>
