2443e31594ccf4f142191953d63aa68d7b599cc4
Luyanda Madonsela <luyandasmadonsela@gmail.com>
2026-07-10 19:18:54 +0200

    Refactor Admin Dashboard layout to use HStack and VStack for improved structure and styling

--- frontend/src/app/admindashboard/page.tsx
+++ frontend/src/app/admindashboard/page.tsx
@@ -1,13 +1,27 @@
-import { VStack } from "@chakra-ui/react/stack";
+import { HStack, VStack } from "@chakra-ui/react/stack";
 import { Text } from "@chakra-ui/react";
 
 
 export default function AdminDashboard() {
   return (
-    <VStack minH="90vh">
-      <Text fontSize="4xl" fontWeight="bold" mb={4}>
-        Admin Dashboard
-      </Text>
-    </VStack>
+    <HStack minH="90vh" w="94%" mt="4rem" alignItems="center">
+      <VStack as="aside" borderWidth="0.2px" borderColor="red.500" w="15%" minH="78.7rem" >
+          <Text fontSize="2xl" ml="1rem">Side Bar</Text>
+      </VStack>
+      <VStack borderWidth="1px" borderColor="red.500" w="85%" minH="70rem">
+        <HStack borderWidth="1px" borderColor="red.500" w="100%" h="4rem">
+          <Text fontSize="2xl"  ml="1rem">Admin Overview</Text>
+        </HStack>
+        <HStack borderWidth="1px" borderColor="red.500" w="100%" h="13rem">
+          <Text fontSize="2xl"  ml="1rem">Stats</Text>
+        </HStack>
+        <HStack borderWidth="1px" borderColor="red.500" w="100%" h="25rem">
+          <Text fontSize="2xl"  ml="1rem">User Activity</Text>
+        </HStack>
+        <HStack borderWidth="1px" borderColor="red.500" w="100%" h="35rem">
+          <Text fontSize="2xl"  ml="1rem">All Users</Text>
+        </HStack>
+      </VStack>
+    </HStack>
   )
 }
