70bd7a8e0bdb01ddaedc2641b96be8e02973e732
Luyanda Madonsela <luyandasmadonsela@gmail.com>
2026-07-10 19:28:18 +0200

    Refactor AdminDashboard layout for improved alignment and structure

--- frontend/src/app/admindashboard/page.tsx
+++ frontend/src/app/admindashboard/page.tsx
@@ -4,11 +4,11 @@ import { Text } from "@chakra-ui/react";
 
 export default function AdminDashboard() {
   return (
-    <HStack minH="90vh" w="94%" mt="4rem" alignItems="center">
-      <VStack as="aside" borderWidth="0.2px" borderColor="red.500" w="15%" minH="78.7rem" >
+    <HStack minH="90vh" w="100%" mt="4.3rem" alignItems="start">
+      <VStack as="aside" borderWidth="0.2px" borderColor="red.500" w="15%" minH="85rem" >
           <Text fontSize="2xl" ml="1rem">Side Bar</Text>
       </VStack>
-      <VStack borderWidth="1px" borderColor="red.500" w="85%" minH="70rem">
+      <VStack borderWidth="1px" borderColor="red.500" w="85%" minH="70rem" justifyContent="start">
         <HStack borderWidth="1px" borderColor="red.500" w="100%" h="4rem">
           <Text fontSize="2xl"  ml="1rem">Admin Overview</Text>
         </HStack>
