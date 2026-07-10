319b07b70cb93c36fae3616b2fae3c18f14e50f6
Luyanda Madonsela <luyandasmadonsela@gmail.com>
2026-07-10 19:34:14 +0200

    Refactor UserDashboard layout to use HStack and add sidebar with navigation elements

--- frontend/src/app/userdashboard/page.tsx
+++ frontend/src/app/userdashboard/page.tsx
@@ -1,12 +1,26 @@
-import { VStack } from "@chakra-ui/react/stack";
+import { HStack, VStack } from "@chakra-ui/react/stack";
 import { Text } from "@chakra-ui/react";
 
 export default function UserDashboard() {
   return (
-    <VStack minH="90vh">
-            <Text fontSize="4xl" fontWeight="bold" mb={4}>
-              User Dashboard
-            </Text>
-    </VStack>
+    <HStack minH="90vh" w="100%" mt="4.3rem" alignItems="start">
+      <VStack as="aside" borderWidth="0.2px" borderColor="red.500" w="15%" minH="85rem" >
+          <Text fontSize="2xl" ml="1rem">Side Bar</Text>
+      </VStack>
+      <VStack borderWidth="1px" borderColor="red.500" w="85%" minH="70rem" justifyContent="start">
+        <HStack borderWidth="1px" borderColor="red.500" w="100%" h="4rem">
+          <Text fontSize="2xl"  ml="1rem">Dashboard</Text>
+        </HStack>
+        <HStack borderWidth="1px" borderColor="red.500" w="100%" h="13rem">
+          <Text fontSize="2xl"  ml="1rem">Stats</Text>
+        </HStack>
+        <HStack borderWidth="1px" borderColor="red.500" w="100%" h="25rem">
+          <Text fontSize="2xl"  ml="1rem">User Profile</Text>
+        </HStack>
+        <HStack borderWidth="1px" borderColor="red.500" w="100%" h="35rem">
+          <Text fontSize="2xl"  ml="1rem">My Favourite Images</Text>
+        </HStack>
+      </VStack>
+    </HStack>
   )
 }
