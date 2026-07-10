5f2aa88b7da07e10d8eb08244000d20f0a6bb8fc
Luyanda Madonsela <luyandasmadonsela@gmail.com>
2026-07-10 10:35:40 +0200

    Width update

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { Box, HStack, Spacer, VStack } from '@chakra-ui/react'
 export default function Footer() {
   return (
         <VStack as="footer" w="maxW" h="27rem" bg="gray.800" color="white" display="flex" alignItems="center" justifyContent="center">
-            <HStack borderWidth="1px" borderColor="red.500" w="110rem" h="13rem" mt="4rem">
+            <HStack borderWidth="1px" borderColor="red.500" w="95%" h="13rem" mt="4rem">
                 <VStack borderWidth="1px" borderColor="red.500" w="20rem" h="13rem" alignItems="center" justifyContent="center">
                     <Box borderWidth="1px" borderColor="red.500" w="20rem" h="3rem"></Box>
                     <Box borderWidth="1px" borderColor="red.500" w="20rem" h="8rem"></Box>
@@ -12,7 +12,7 @@ export default function Footer() {
                 </VStack>
             </HStack>
             <Spacer/>
-            <HStack borderWidth="1px" borderColor="red.500" w="110rem" h="3.12rem" mb="2.5rem">
+            <HStack borderWidth="1px" borderColor="red.500" w="95%" h="3.12rem" mb="2.5rem">
                 <Box borderWidth="1px" borderColor="red.500" w="25rem" h="3rem"></Box>
                 <Spacer/>
                 <Box borderWidth="1px" borderColor="red.500" w="30rem" h="3rem"></Box>
