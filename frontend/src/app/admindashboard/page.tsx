0b885051687b15fa5b004ed59e264747eb80304f
Luyanda Madonsela <luyandasmadonsela@gmail.com>
2026-07-11 17:25:16 +0200

    Add sidebar logo and update Admin Dashboard layout with improved styling

--- frontend/src/app/admindashboard/page.tsx
+++ frontend/src/app/admindashboard/page.tsx
@@ -1,15 +1,32 @@
+"use client";
+
 import { HStack, VStack } from "@chakra-ui/react/stack";
-import { Text } from "@chakra-ui/react";
+import { Text, Link } from "@chakra-ui/react";
+import NextLink from "next/link";
+import Image from "next/image";
+import { Box } from '@chakra-ui/react';
 
 
 export default function AdminDashboard() {
   return (
     <HStack minH="90vh" w="94%" mt="4rem" alignItems="center">
-      <VStack as="aside" borderWidth="0.2px" borderColor="red.500" w="15%" minH="78.7rem" >
-          <Text fontSize="2xl" ml="1rem">Side Bar</Text>
+      <VStack as="aside" w="15%" minH="78.7rem"  borderRightWidth="0.2px" borderRightColor="#E2E8F0" >
+            <Link as={NextLink} href="/" _hover={{ textDecoration: "none" }}>
+                <HStack ml="12rem" w="25rem" h="3rem">
+                  <Box w="10%" h="2rem" display="flex" alignItems="center" justifyContent="center" borderRadius="0.375rem" bgColor="#2563EB">
+                    <Image src="/zap.svg" alt="logo" width={20} height={20} />
+                  </Box>
+                  <Text fontSize="lg" fontWeight="bold"> Practice Project </Text>
+                </HStack>
+            </Link>
+
+
+
+
+            
       </VStack>
-      <VStack borderWidth="1px" borderColor="red.500" w="85%" minH="70rem">
-        <HStack borderWidth="1px" borderColor="red.500" w="100%" h="4rem">
+      <VStack w="85%" minH="70rem">
+        <HStack borderBottomWidth="1px" borderBottomColor="#E2E8F0" w="100%" h="4rem">
           <Text fontSize="2xl"  ml="1rem">Admin Overview</Text>
         </HStack>
         <HStack borderWidth="1px" borderColor="red.500" w="100%" h="13rem">
