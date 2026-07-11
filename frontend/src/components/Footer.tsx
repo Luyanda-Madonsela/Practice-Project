969fb1b81a40343f72f8317bcec54988e29253cc
Luyanda Madonsela <luyandasmadonsela@gmail.com>
2026-07-11 10:43:18 +0200

    Refactor footer component to include logo and project title

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
-import { Box, HStack, Spacer, VStack } from '@chakra-ui/react'
+import { Box, HStack, Spacer, VStack, Text } from '@chakra-ui/react'
+import Image from 'next/image'
+import Logo from "../../public/zap.svg";
 
 export default function Footer() {
   return (
         <VStack as="footer" w="maxW" h="27rem" bg="gray.800" color="white" display="flex" alignItems="center" justifyContent="center">
             <HStack borderWidth="1px" borderColor="red.500" w="90%" h="13rem" mt="4rem">
                 <VStack borderWidth="1px" borderColor="red.500" w="20rem" h="13rem" alignItems="center" justifyContent="center">
-                    <Box borderWidth="1px" borderColor="red.500" w="20rem" h="3rem"></Box>
+                    <Box borderWidth="1px" borderColor="red.500" w="20rem" h="3rem">
+                        <HStack w="20rem" h="3rem">
+                            <Box w="10%" h="2rem" display="flex" alignItems="center" justifyContent="center" bgColor="blue.500">
+                              <Image src={Logo} alt="logo" width={20} height={20} />
+                            </Box>
+                            <Text fontSize="lg" fontWeight="bold"> Practice Project </Text>
+                        </HStack>
+                    </Box>
                     <Box borderWidth="1px" borderColor="red.500" w="20rem" h="8rem"></Box>
                     <Box borderWidth="1px" borderColor="red.500" w="20rem" h="2rem"></Box>
                 </VStack>
